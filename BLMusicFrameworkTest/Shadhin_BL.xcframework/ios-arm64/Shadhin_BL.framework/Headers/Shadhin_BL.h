27bb6018d6424bade4f17827b9822dabd247af68
MD Azizur Rahman <azizur.gakk@gmail.com>
2023-02-12 14:41:27 +0600

    prep for v 0.0.11

---
+++ BLMusicFrameworkTest/Shadhin_BL.xcframework/ios-arm64/Shadhin_BL.framework/Headers/Shadhin_BL.h
@@ -0,0 +1,19 @@
+//
+//  Shadhin_BL.h
+//  Shadhin-BL
+//
+//  Created by Joy on 21/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Shadhin_BL.
+FOUNDATION_EXPORT double Shadhin_BLVersionNumber;
+
+//! Project version string for Shadhin_BL.
+FOUNDATION_EXPORT const unsigned char Shadhin_BLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shadhin_BL/PublicHeader.h>
+
+
+#import "LNPopupController.h"
